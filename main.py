c4fc5c128b571f4d72fb772b0aff1c6888519ee8
Harsh R. Saxena <harsh.saxena@gmail.com>
2026-06-02 14:13:15 +0530

    conditional list comprehension testing

--- main.py
+++ main.py
@@ -13,3 +13,9 @@ print(new_list)
 names = ["Kaju", "Tim", "Johnny", "Jill", "Jack", "Danny", "Jonathan"]
 short_names = [name for name in names if len(name) < 5]
 print(short_names)
+
+long_names = [name.upper() for name in names if len(name) > 4]
+print(long_names)
+
+long_names = [name.lower() for name in names if len(name) > 4]
+print(long_names)
